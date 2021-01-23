79b6117627d0f3a59e45ac33668eb43e943e4720
Matt Galvin <matthewgalvin908@gmail.com>
2021-01-23 21:52:31 +0100

    Add test/support directory b/c the book incorrectly tells you to use * a function from csv_test.exs in employee_test.exs

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule Bday.MixProject do
       app: :bday,
       version: "0.1.0",
       elixir: "~> 1.7",
+      elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,4 +26,9 @@ defmodule Bday.MixProject do
       {:propcheck, "~> 1.1", only: [:test, :dev]}
     ]
   end
+
+  # Specifies which paths to compile per environment.
+  # https://adamdelong.com/elixir-module-not-available/
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 end
