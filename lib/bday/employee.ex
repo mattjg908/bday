220b47b11212106d143e3ac2f2b57d97f910755c
Matt Galvin <matthewgalvin908@gmail.com>
2021-01-24 12:26:02 +0100

    Make date formatting property green

--- lib/bday/employee.ex
+++ lib/bday/employee.ex
@@ -10,15 +10,25 @@ defmodule Bday.Employee do
   end
 
   defp adapt_csv_result(map) do
-    for {k, v} <- map, into: %{} do
-      {trim(k), maybe_null(trim(v))}
-    end
+    map =
+      for {k, v} <- map, into: %{} do
+        {trim(k), maybe_null(trim(v))}
+      end
+
+    dob = Map.fetch!(map, "date_of_birth")
+    %{map | "date_of_birth" => parse_date(dob)}
   end
 
-  defp trim(str), do: String.trim_leading(str, " ")
+  defp trim(str), do: String.trim_leading(str)
 
   defp maybe_null(""), do: nil
   defp maybe_null(str), do: str
+
+  defp parse_date(str) do
+    [y, m, d] = Enum.map(String.split(str, "/"), &String.to_integer(&1))
+    {:ok, date} = Date.new(y, m, d)
+    date
+  end
 
   if Mix.env() == :test do
     def adapt_csv_result_shim(map), do: adapt_csv_result(map)
