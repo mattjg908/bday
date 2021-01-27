492899b17c3c16caac850d9e88b96a50ad96d667
Matt Galvin <matthewgalvin908@gmail.com>
2021-01-27 09:55:31 +0100

    Add function for extracting e-mail related information

--- lib/bday/mail_tpl.ex
+++ lib/bday/mail_tpl.ex
@@ -3,4 +3,8 @@ defmodule Bday.MailTpl do
     name = Bday.Employee.first_name(employee)
     "Happy birthday, dear #{name}!"
   end
+
+  def full(employee) do
+    {[Bday.Employee.email(employee)], "Happy birthday!", body(employee)}
+  end
 end
